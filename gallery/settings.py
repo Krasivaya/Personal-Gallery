505c4199ab66c49c12becb51357ab8459575a07a
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-14 13:00:56 +0200

    Add database infos

--- gallery/settings.py
+++ gallery/settings.py
@@ -76,7 +76,9 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'gallery',
+        'USER': 'wecode',
+        'PASSWORD': 'wecode',
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Kigali'
 
 USE_I18N = True
 
