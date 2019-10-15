dec9ed8de435ca7de0630c0b5b5867d6ad097401
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-15 12:40:54 +0200

    Add models to admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Location, Category, Image
 
 
-# Register your models here.
+# Register your models here. 
 
 admin.site.register(Location)
 admin.site.register(Category)
